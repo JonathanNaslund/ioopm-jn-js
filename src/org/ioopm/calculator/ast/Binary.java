a58bbcf0cc6050aaf1fbabb8400e2597cf74853f
root <root@JonaLaptop.localdomain>
2023-11-15 12:57:46 +0100

    update on changePriority

--- src/org/ioopm/calculator/ast/Binary.java
+++ src/org/ioopm/calculator/ast/Binary.java
@@ -14,33 +14,17 @@ public abstract class Binary extends SymbolicExpression {
     }
 
     private void changePriority() {
-        if (this.name == "*" || this.name == "/") {
-            if (!lhs.isAtom() && !rhs.isAtom()) {
-                String lhsSymbol = lhs.getName();
-                String rhsSymbol = rhs.getName();
-                if ((lhsSymbol == "+" || lhsSymbol == "-") && (rhsSymbol == "+" || rhsSymbol == "-")) {
-                    lhs.setPriority(3);
-                    rhs.setPriority(3);
-                } else if (lhsSymbol == "+"|| lhsSymbol == "-") {
-                    lhs.setPriority(3);
-                } else if (rhsSymbol == "+"|| rhsSymbol == "-") {
-                    rhs.setPriority(3);
-                }
-            } else if (!lhs.isAtom()) {
-                String lhsSymbol = lhs.getName();
-                if (lhsSymbol == "+"|| lhsSymbol == "-") {
-                    lhs.setPriority(3);
-                }
-            } else if (!rhs.isAtom()) {
-                String rhsSymbol = rhs.getName();
-                if (rhsSymbol == "+"|| rhsSymbol == "-") {
-                    lhs.setPriority(3);
-                }
-            }
-        } else if (this.name == "-" && !rhs.isAtom()) {
-            if (rhs.getName() == "+") {
+        if (this instanceof Multiplication || this instanceof Division) {
+            if ((lhs instanceof Addition || lhs instanceof Subtraction) && (rhs instanceof Addition || rhs instanceof Subtraction)) {
+                lhs.setPriority(3);
+                rhs.setPriority(3);
+            } else if (lhs instanceof Addition || lhs instanceof Subtraction) {
+                lhs.setPriority(3);
+            } else if (rhs instanceof Addition || rhs instanceof Subtraction) {
                 rhs.setPriority(3);
             }
+        } else if (this instanceof Subtraction && rhs instanceof Addition) {
+            rhs.setPriority(3);
         }
     }
 
